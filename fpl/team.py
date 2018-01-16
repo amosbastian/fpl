3625b365df0f12246b2ab3e507825a662513311c
amosbastian <amos_bastian@hotmail.com>
2018-01-16 16:41:23 +0000

    Update Team

--- fpl/team.py
+++ fpl/team.py
@@ -1,8 +1,6 @@
 import json
 import requests
 
-API_BASE_URL = "https://fantasy.premierleague.com/drf/"
-
 class Team(object):
     """
     A class representing a real team in the Fantasy Premier League.
