cd1ba9b43b1a49b9a200a19e3abfd32fcb46f308
amosbastian <amos_bastian@hotmail.com>
2018-01-31 15:07:44 +0000

    Gameweek object with attributes

--- fpl/gameweek.py
+++ fpl/gameweek.py
@@ -7,14 +7,41 @@ class Gameweek(object):
     """
     A class representing a gameweek of the Fantasy Premier League.
     """
-    def __init__(self, gameweek):
-        self.__dict__ = gameweek
+    def __init__(self, gameweek_id):
+        self.id = gameweek_id
+        
+        self._additional = self._additional()
+        self._specific = self._specific()
 
-    @property
-    def additional(self):
-        return requests.get("{}event/{}/live".format(API_BASE_URL,
-            self.id)).json()
+        # General gameweek information
+        self.deadline = self._specific["deadline_time"]
+        self.finished = self._specific["finished"]
+        self.name = self._specific["name"]
+
+        self.is_current = self._specific["is_current"]
+        self.is_next = self._specific["is_next"]
+        self.is_previous = self._specific["is_previous"]
+
+        # Gameweek score information
+        self.average_score = self._specific["average_entry_score"]
+        self.best_player = self._specific["highest_scoring_entry"]
+        self.highest_score = self._specific["highest_score"]
 
     @property
     def fixtures(self):
-        return self.additional["fixtures"]
+        return self._additional["fixtures"]
+
+    @property
+    def players(self):
+        return self._additional["players"]
+
+    def _specific(self):
+        response = requests.get("{}events".format(API_BASE_URL)).json()
+        return response[self.id - 1]
+
+    def _additional(self):
+        return requests.get("{}event/{}/live".format(API_BASE_URL,
+            self.id)).json()
+
+if __name__ == '__main__':
+    print(json.dumps(Gameweek(1)._additional["fixtures"]))
