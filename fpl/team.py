229f97a278266e6408b8baa4befc73a5166c48e2
amosbastian <amos_bastian@hotmail.com>
2018-01-31 14:48:14 +0000

    Team object now requests itself

--- fpl/team.py
+++ fpl/team.py
@@ -1,9 +1,25 @@
 import json
 import requests
 
+API_BASE_URL = "https://fantasy.premierleague.com/drf/"
+
 class Team(object):
     """
     A class representing a real team in the Fantasy Premier League.
     """
-    def __init__(self, team):
-        self.__dict__ = team
+    def __init__(self, team_id):
+        self.id = team_id
+        self._information = self._information()
+
+        self.name = self._information["name"]
+        self.short_name = self._information["short_name"]
+
+        self.current_fixture = self._information["current_event_fixture"]
+        self.next_fixture = self._information["next_event_fixture"]
+
+    def _information(self):
+        response = requests.get("{}teams".format(API_BASE_URL)).json()
+        return response[self.id - 1]
+
+if __name__ == '__main__':
+    print(json.dumps(Team(1)._information))
