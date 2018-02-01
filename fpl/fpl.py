63b0526656624e78408f6df534391c77e532c3f5
amosbastian <amos_bastian@hotmail.com>
2018-02-01 17:33:58 +0000

    Update fpl.py documentation

--- fpl/fpl.py
+++ fpl/fpl.py
@@ -13,7 +13,7 @@ API_BASE_URL = "https://fantasy.premierleague.com/drf/"
 class FPL():
     def get_user(self, user_id):
         """
-        Returns a User object containing information about the user with the 
+        Returns a `User` object containing information about the user with the 
         given `user_id`.
 
         :param string user_id: A user's id
@@ -22,45 +22,45 @@ class FPL():
 
     def get_teams(self):
         """
-        Returns a list of Team objects of the teams currently participating 
+        Returns a list of `Team` objects of the teams currently participating 
         in the Premier League.
         """
         return[Team(team_id) for team_id in range(1, 20)]
 
     def get_team(self, team_id):
         """
-        Returns a Team object containing information about the team with the 
+        Returns a `Team` object containing information about the team with the 
         given `team_id`.
 
         :param string user_id: A team's id
 
-        ```
-         1 - Arsenal
-         2 - Bournemouth
-         3 - Brighton
-         4 - Burnley
-         5 - Chelsea
-         6 - Crystal Palace
-         7 - Everton
-         8 - Huddersfield
-         9 - Leicester
-        10 - Liverpool
-        11 - Man City
-        12 - Man Utd
-        13 - Newcastle
-        14 - Southampton
-        15 - Stoke
-        16 - Swansea
-        17 - Spurs
-        18 - Watford
-        19 - West Brom
-        ```
+        .. code-block:: none
+
+             1 - Arsenal
+             2 - Bournemouth
+             3 - Brighton
+             4 - Burnley
+             5 - Chelsea
+             6 - Crystal Palace
+             7 - Everton
+             8 - Huddersfield
+             9 - Leicester
+            10 - Liverpool
+            11 - Man City
+            12 - Man Utd
+            13 - Newcastle
+            14 - Southampton
+            15 - Stoke
+            16 - Swansea
+            17 - Spurs
+            18 - Watford
+            19 - West Brom
         """
         return Team(team_id)
 
     def get_players(self):
         """
-        Returns a list of information about all players currently playing for 
+        Returns a list of `Player` objects of all players currently playing for 
         teams in the Premier League.
         """
         players = []
@@ -72,7 +72,7 @@ class FPL():
 
     def get_player(self, player_id):
         """
-        Returns a Player object of the player with the given `player_id`.
+        Returns a `Player` object of the player with the given `player_id`.
 
         :param int player_id: A player's id
         """
@@ -80,13 +80,13 @@ class FPL():
 
     def get_gameweeks(self):
         """
-        Returns a list Gameweek objects.
+        Returns a list `Gameweek` objects.
         """
         return [Gameweek(gameweek_id) for gameweek_id in range(1, 38)]
 
     def get_gameweek(self, gameweek_id):
         """
-        Returns a Gameweek object of the specified gameweek.
+        Returns a `Gameweek` object of the specified gameweek.
 
         :param int gameweek: The gameweek (1-38)
         """
@@ -100,7 +100,7 @@ class FPL():
 
     def get_classic_league(self, league_id):
         """
-        Returns a ClassicLeague object from the given `league_id`.
+        Returns a `ClassicLeague` object with the given `league_id`.
 
         :param string league_id: A league's id
         """
@@ -108,7 +108,7 @@ class FPL():
 
     def get_h2h_league(self, league_id):
         """
-        Returns a H2HLeague object from the given `league_id`.
+        Returns a `H2HLeague` object with the given `league_id`.
 
         :param string league_id: A league's id
         """
