60bfdb37badb8f454ce4baecd7731c9705c99627
amosbastian <amos_bastian@hotmail.com>
2018-02-01 17:57:41 +0000

    Add docstrings and change fixtures

--- fpl/team.py
+++ fpl/team.py
@@ -11,11 +11,19 @@ class Team(object):
         self.id = team_id
         self._information = self._information()
 
+        #: The name of the team, e.g. "Arsenal".
         self.name = self._information["name"]
+        #: The short name of the team, e.g. "ARS".
         self.short_name = self._information["short_name"]
 
-        self.current_fixture = self._information["current_event_fixture"]
-        self.next_fixture = self._information["next_event_fixture"]
+        self.current_fixture = self._information["current_event_fixture"][0]
+        """
+        A dictionary containing information about the team's current fixture.
+        """
+        self.next_fixture = self._information["next_event_fixture"][0]
+        """
+        A dictionary containing information about the team's next fixture.
+        """
 
     def _information(self):
         response = requests.get("{}teams".format(API_BASE_URL)).json()
@@ -23,3 +31,7 @@ class Team(object):
 
     def __str__(self):
         return self.name
+
+if __name__ == '__main__':
+    team = Team(1)
+    print(team.next_fixture)
