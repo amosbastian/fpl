1d79701e1dac4d3e47ed6ce36b4dae9a83cc373c
amosbastian <amos_bastian@hotmail.com>
2018-02-01 17:50:39 +0000

    Add docstrings and fix bug

--- fpl/gameweek.py
+++ fpl/gameweek.py
@@ -13,27 +13,41 @@ class Gameweek(object):
         self._additional = self._additional()
         self._specific = self._specific()
 
-        # General gameweek information
+        #: A `datetime` object of the gameweek's deadline.
         self.deadline = self._specific["deadline_time"]
+        #: A boolean that signifies if the gameweek is already finished.
         self.finished = self._specific["finished"]
+        #: The name of the gameweek, e.g. "Gameweek 1".
         self.name = self._specific["name"]
 
+        #: A boolean that signifies if the gameweek is the current gameweek.
         self.is_current = self._specific["is_current"]
+        #: A boolean that signifies if the gameweek is the next gameweek.
         self.is_next = self._specific["is_next"]
+        #: A boolean that signifies if the gameweek is the previous gameweek.
         self.is_previous = self._specific["is_previous"]
 
-        # Gameweek score information
+        #: The average score of the gameweek.
         self.average_score = self._specific["average_entry_score"]
+        #: The `user_id` of the best player of the gameweek.
         self.best_player = self._specific["highest_scoring_entry"]
+        #: The highest score of the gameweek.
         self.highest_score = self._specific["highest_score"]
 
     @property
     def fixtures(self):
+        """
+        A list of dictionaries containing information about the fixtures of
+        the gameweek.
+        """
         return self._additional["fixtures"]
 
     @property
     def players(self):
-        return self._additional["players"]
+        """
+        Returns a dictionary containing all players that played in the gameweek.
+        """
+        return self._additional["elements"]
 
     def _specific(self):
         response = requests.get("{}events".format(API_BASE_URL)).json()
@@ -45,3 +59,7 @@ class Gameweek(object):
 
     def __str__(self):
         return "{} - {}".format(self.name, self.deadline)
+
+if __name__ == '__main__':
+    gameweek = Gameweek(1)
+    print(json.dumps(gameweek._additional))
