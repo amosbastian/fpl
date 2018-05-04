5efefdb7d0ba5f6490e6e510af526c2450f73993
amosbastian <amos_bastian@hotmail.com>
2018-05-04 23:37:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,23 @@ from setuptools import setup
 
 setup(
     name="fpl",
-    version="0.3.3",
+    version="0.4.0",
     packages=["fpl"],
     description="A Python wrapper for the Fantasy Premier League API",
     url="https://github.com/amosbastian/fpl",
     author="amosbastian",
     author_email="amosbastian@gmail.com",
-    license="MIT"
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ],
+    keywords="fpl fantasy premier league",
+    project_urls={
+        "Documentation": "http://fpl.readthedocs.io/en/latest/",
+        "Source": "https://github.com/amosbastian/fpl"
+    },
+    install_requires=["requests"]
 )
