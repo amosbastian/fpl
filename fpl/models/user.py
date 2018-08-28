19bb4adc419b65bc590e9c0d7308626b840969f6
amosbastian <amosbastian@gmail.com>
2018-08-28 23:15:58 +0100

    Add additional properties to User class
    Once logged in the User class can now access:
    * the User's current team
    * the User's watchlist

--- fpl/models/user.py
+++ fpl/models/user.py
@@ -1,14 +1,20 @@
 import requests
 
-API_BASE_URL = "https://fantasy.premierleague.com/drf/"
+from ..constants import API_URLS
+
+
+def valid_gameweek(gameweek):
+    """Returns True if the gameweek is valid."""
+    if not isinstance(gameweek, int) and (gameweek < 1 or gameweek > 38):
+        raise("Gameweek must be a number between 1 and 38.")
+    return True
 
 
 class User(object):
-    """
-    A class representing a user of the Fantasy Premier League.
-    """
-    def __init__(self, user_id):
+    """A class representing a user of the Fantasy Premier League."""
+    def __init__(self, user_id, session):
         self.id = user_id
+        self._session = session
         self._information = self._get_information()
         self._entry = self._information["entry"]
 
@@ -62,129 +68,195 @@ class User(object):
         #: The user's cup matches.
         self.cup_matches = self._information["cup_matches"]
 
+        #: Account deletion status.
+        self.deleted = self._entry["deleted"]
+
     def _get_information(self):
-        """
-        Returns some general information about the user.
-        """
-        return requests.get(
-            "{}entry/{}/cup".format(API_BASE_URL, self.id)).json()
+        """Returns some general information about the user."""
+        return requests.get(API_URLS["user_cup"].format(self.id)).json()
 
     @property
     def history(self):
-        """
-        Returns a dictionary containing the history of the user.
-        """
-        return requests.get(
-            "{}entry/{}/history".format(API_BASE_URL, self.id)).json()
+        """Returns a dictionary containing the history of the user."""
+        return requests.get(API_URLS["user_history"].format(self.id)).json()
 
     @property
-    def season(self):
-        """
-        Returns a list containing information about each of the seasons the
+    def season_history(self):
+        """Returns a list containing information about each of the seasons the
         user has participated in.
         """
         return self.history["season"]
 
     @property
     def chips(self):
-        """
-        Returns a list containing information about the usage of the player's
-        chips.
+        """Returns a list containing information about the usage of the
+        player's chips.
         """
         return self.history["chips"]
 
     @property
     def leagues(self):
-        """
-        Returns a dictionary containing information about all the leagues that
-        the user is participating in.
+        """Returns a dictionary containing information about all the leagues
+        that the user is participating in.
         """
         return self.history["leagues"]
 
     @property
     def classic(self):
-        """
-        Returns a list containing information about all the classic leagues
+        """Returns a list containing information about all the classic leagues
         that the user is currently participating in.
         """
         return self.leagues["classic"]
 
     @property
     def h2h(self):
-        """
-        Returns a list containing information about all the h2h leagues that
+        """Returns a list containing information about all the h2h leagues that
         the user is currently participating in.
         """
         return self.leagues["h2h"]
 
     @property
     def picks(self):
-        """
-        Returns a dictionary containing information about the user's chip
+        """Returns a dictionary containing information about the user's chip
         usage, automatic substitutions and picks, alongside general
         information about each gameweek.
         """
         picks = {}
         for gameweek in range(1, 39):
-            pick = requests.get("{}entry/{}/event/{}/picks".format(
-                API_BASE_URL, self.id, gameweek))
+            team = requests.get(API_URLS["user_picks"].format(
+                self.id, gameweek))
 
-            if pick.status_code == 404:
+            if team.status_code == 404:
                 return picks
 
-            picks[gameweek] = pick.json()
+            picks[gameweek] = team.json()
 
         return picks
 
-    def team(self, gameweek):
-        """
-        Returns a list of the user's team in the specified gameweek.
+    def my_team(self):
+        """Returns a logged in user's current team."""
+        if not self._session:
+            raise("User must be logged in.")
 
-        :param int player_id: The gameweek (1-38)
-        """
-        return self.picks[gameweek]["picks"]
+        response = self._session.get(API_URLS["user_team"].format(self.id))
+        return response.json()["picks"]
 
-    def chip(self, gameweek):
-        """
-        Returns the chip used by the user in the specified gameweek.
+    def team(self, gameweek=None):
+        """Returns a list of all of the user's teams so far, or the user's team
+        in the specified gameweek.
 
-        :param int player_id: The gameweek (1-38)
+        :param int gameweek: A gameweek (1-38)
         """
-        return self.picks[gameweek]["active_chip"]
+        if gameweek:
+            valid_gameweek(gameweek)
+            return self.picks[gameweek]["picks"]
 
-    def automatic_substitutions(self, gameweek):
-        """
-        Returns a list of the automatic substitutions of the user in the
-        specified gameweek.
+        teams = []
+        for gameweek in range(1, 39):
+            try:
+                team = self.picks[gameweek]["picks"]
+                teams.append(team)
+            except KeyError:
+                return teams
 
-        :param int player_id: The gameweek (1-38)
+        return teams
+
+    def chip(self, gameweek=None):
+        """Returns a list of chips used by the user so far, or the chip used
+        by the user in the specified gameweek.
+
+        :param int gameweek: A gameweek (1-38)
         """
-        return self.picks[gameweek]["automatic_subs"]
+        if gameweek:
+            valid_gameweek(gameweek)
+            return self.picks[gameweek]["active_chip"]
+
+        active_chips = []
+        for gameweek in range(1, 39):
+            try:
+                active_chip = self.picks[gameweek]["active_chip"]
+                active_chips.append(active_chip)
+            except KeyError:
+                return active_chips
+
+        return active_chips
+
+    def automatic_substitutions(self, gameweek=None):
+        """Returns a list of all automatic substitions made for the user so
+        far, or the automatic substitutions made for the user in the specified
+        gameweek.
+
+        :param int gameweek: A gameweek (1-38)
+        """
+        if gameweek:
+            valid_gameweek(gameweek)
+            return self.picks[gameweek]["automatic_subs"]
+
+        automatic_substitutions = []
+        for gameweek in range(1, 39):
+            try:
+                automatic_substitution = self.picks[gameweek]["automatic_subs"]
+                automatic_substitutions.append(automatic_substitution)
+            except KeyError:
+                return automatic_substitutions
+
+        return automatic_substitutions
+
+    def gameweek_history(self, gameweek=None):
+        """Returns a list of the user's history per gameweek, or the history
+        of a specific gameweek.
+
+        :param int gameweek: A gameweek (1-38)
+        """
+        if gameweek:
+            valid_gameweek(gameweek)
+            return self.picks[gameweek]["entry_history"]
+
+        histories = []
+        for gameweek in range(1, 39):
+            try:
+                history = self.picks[gameweek]["entry_history"]
+                histories.append(history)
+            except KeyError:
+                return histories
+
+        return history
 
     @property
     def transfers(self):
+        """Returns a dictionary containing information about all the transfers
+        the user has made so far.
         """
-        Returns a dictionary containing information about all the transfers the
-        user has made so far.
-        """
-        return requests.get(
-            "{}entry/{}/transfers".format(API_BASE_URL, self.id)).json()
+        return requests.get(API_URLS["user_transfers"].format(self.id)).json()
 
     @property
     def wildcards(self):
-        """
-        Returns a list containing information about the usage of the player's
-        wildcard(s).
+        """Returns a list containing information about the usage of the
+        player's wildcard(s).
         """
         return self.transfers["wildcards"]
 
-    @property
-    def transfer_history(self):
-        """
-        Returns a list containing information about the user's transfer
+    def transfer_history(self, gameweek=None):
+        """Returns a list containing information about the user's transfer
         history.
+
+        :param int gameweek: A gameweek (1-38)
         """
+        if gameweek:
+            valid_gameweek(gameweek)
+            transfers = [transfer for transfer in self.transfers["history"]
+                         if transfer["event"] == gameweek]
+            return transfers
+
         return self.transfers["history"]
+
+    @property
+    def watchlist(self):
+        """Returns the user's watchlist."""
+        if not self._session:
+            raise("User must be logged in.")
+
+        return self._session.get(API_URLS["watchlist"]).json()
 
     def __str__(self):
         return "{} {} - {}".format(
