e235562cfc510f28d526fad933bf2f9d34cd8ee5
amosbastian <amosbastian@gmail.com>
2018-08-29 22:24:04 +0100

    Update unit tests for FPL class

--- tests/test_fpl.py
+++ tests/test_fpl.py
@@ -18,38 +18,47 @@ class FPLTest(unittest.TestCase):
 
     def test_user(self):
         user = self.fpl.get_user("3523615")
-        self.assertTrue(isinstance(user, User))
+        self.assertIsInstance(user, User)
+
+    def test_team(self):
+        team = self.fpl.get_team(1)
+        self.assertIsInstance(team, Team)
 
     def test_teams(self):
         teams = self.fpl.get_teams()
-        self.assertEqual(isinstance(teams, list), True)
+        self.assertIsInstance(teams, list)
         self.assertEqual(len(teams), 20)
+        self.assertIsInstance(teams[0], Team)
+
+    def test_player(self):
+        player = self.fpl.get_player(1)
+        self.assertIsInstance(player, Player)
 
     def test_players(self):
         players = self.fpl.get_players()
-        self.assertTrue(isinstance(players, list))
-        self.assertTrue(isinstance(players[0], Player))
+        self.assertIsInstance(players, list)
+        self.assertIsInstance(players[0], Player)
 
     def test_gameweeks(self):
         gameweeks = self.fpl.get_gameweeks()
-        self.assertTrue(isinstance(gameweeks, list))
+        self.assertIsInstance(gameweeks, list)
         self.assertEqual(len(gameweeks), 38)
 
     def test_gameweek(self):
         gameweek = self.fpl.get_gameweek("20")
-        self.assertTrue(isinstance(gameweek, Gameweek))
+        self.assertIsInstance(gameweek, Gameweek)
 
     def test_game_settings(self):
         game_settings = self.fpl.game_settings()
-        self.assertEqual(isinstance(game_settings, dict), True)
+        self.assertIsInstance(game_settings, dict)
 
     def test_classic_league(self):
         classic_league = self.fpl.get_classic_league("890172")
-        self.assertTrue(isinstance(classic_league, ClassicLeague))
+        self.assertIsInstance(classic_league, ClassicLeague)
 
     def test_h2h_league(self):
         h2h_league = self.fpl.get_h2h_league("760869")
-        self.assertTrue(isinstance(h2h_league, H2HLeague))
+        self.assertIsInstance(h2h_league, H2HLeague)
 
 
 if __name__ == '__main__':
