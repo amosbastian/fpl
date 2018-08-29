ae0a8fac1479e8575092b858982b0b27fbc44fc9
amosbastian <amosbastian@gmail.com>
2018-08-29 22:25:11 +0100

    Update Team class, add unit tests
    Added additional properties to the Team class that were missing
    before and add a function to retrieve a list of players that
    are currently playing for the team.

---
+++ tests/test_team.py
@@ -0,0 +1,28 @@
+import unittest
+import warnings
+
+from fpl import FPL
+from fpl.models.player import Player
+
+
+class TeamTest(unittest.TestCase):
+    def setUp(self):
+        self.fpl = FPL()
+        warnings.filterwarnings("ignore", category=ResourceWarning,
+                                message="unclosed.*<ssl.SSLSocket.*>")
+        self.team = self.fpl.get_team(1)
+
+    def test_team(self):
+        self.assertEqual(self.team.__str__(), self.team.name)
+
+    def test__get_information(self):
+        information = self.team._get_information()
+        self.assertIsInstance(information, dict)
+
+    def test_get_players(self):
+        self.team.get_players()
+        self.assertIsInstance(self.team.players, list)
+        self.assertIsInstance(self.team.players[0], Player)
+
+if __name__ == '__main__':
+    unittest.main()
