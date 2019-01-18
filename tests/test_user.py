e057ef94a3cdd2a4c9fe00e2944c4c1de5958720
amosbastian <amosbastian@gmail.com>
2019-01-18 13:51:07 +0000

    Update User class unit tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,8 +10,11 @@ class UserTest(unittest.TestCase):
         _run(self.fpl.login())
         self.user = _run(self.fpl.get_user("3808385"))
 
-    def test_history(self):
-        history = _run(self.user.get_history())
+    def test_gameweek_history(self):
+        history = _run(self.user.get_gameweek_history())
+        self.assertIsInstance(history, list)
+
+        history = _run(self.user.get_gameweek_history(1))
         self.assertIsInstance(history, dict)
 
     def test_season_history(self):
@@ -23,64 +26,49 @@ class UserTest(unittest.TestCase):
         self.assertIsInstance(chips, list)
 
     def test_leagues(self):
-        leagues = _run(self.user.get_leagues())
+        leagues = self.user.leagues
         self.assertIsInstance(leagues, dict)
-
-    def test_classic(self):
-        classic = _run(self.user.get_classic_leagues())
-        self.assertIsInstance(classic, list)
-
-    def test_h2h(self):
-        h2h = _run(self.user.get_h2h_leagues())
-        self.assertIsInstance(h2h, list)
 
     def test_picks(self):
         picks = _run(self.user.get_picks())
         self.assertIsInstance(picks, list)
-        self.assertEqual(len(picks), self.user.current_gameweek)
+        self.assertEqual(len(picks), self.user.current_event)
 
-    def test_my_team(self):
-        my_team = _run(self.user.my_team())
-        self.assertIsInstance(my_team, list)
+        picks = _run(self.user.get_picks(1))
+        self.assertIsInstance(picks, list)
 
-    def test_team(self):
-        team = _run(self.user.get_team())
-        self.assertIsInstance(team, list)
-        team = _run(self.user.get_team(gameweek=1))
-        self.assertIsInstance(team, list)
+    def test_active_chips(self):
+        active_chips = _run(self.user.get_active_chips())
+        self.assertIsInstance(active_chips, list)
+        self.assertEqual(len(active_chips), self.user.current_event)
 
-    def test_chip(self):
-        chip = _run(self.user.get_chips())
-        self.assertIsInstance(chip, list)
-        chip = _run(self.user.get_chips(gameweek=1))
-        self.assertEqual(chip, "")
+        active_chips = _run(self.user.get_active_chips(1))
+        self.assertIsInstance(active_chips, list)
 
     def test_automatic_substitutions(self):
         automatic_substitutions = _run(self.user.get_automatic_substitutions())
         self.assertIsInstance(automatic_substitutions, list)
+        self.assertEqual(len(automatic_substitutions),
+                         self.user.current_event)
+
         automatic_substitutions = _run(
-            self.user.get_automatic_substitutions(gameweek=1))
+            self.user.get_automatic_substitutions(1))
         self.assertIsInstance(automatic_substitutions, list)
 
-    def test_gameweek_history(self):
-        gameweek_history = _run(self.user.get_gameweek_history())
-        self.assertIsInstance(gameweek_history, list)
-        gameweek_history = _run(self.user.get_gameweek_history(gameweek=1))
-        self.assertIsInstance(gameweek_history, dict)
+    def test_team(self):
+        my_team = _run(self.user.get_team())
+        self.assertIsInstance(my_team, list)
 
     def test_transfers(self):
         transfers = _run(self.user.get_transfers())
-        self.assertIsInstance(transfers, dict)
+        self.assertIsInstance(transfers, list)
+
+        transfers = _run(self.user.get_transfers(1))
+        self.assertIsInstance(transfers, list)
 
     def test_wildcards(self):
         wildcards = _run(self.user.get_wildcards())
         self.assertIsInstance(wildcards, list)
-
-    def test_transfer_history(self):
-        transfer_history = _run(self.user.get_transfer_history())
-        self.assertIsInstance(transfer_history, list)
-        transfer_history = _run(self.user.get_transfer_history(gameweek=1))
-        self.assertIsInstance(transfer_history, list)
 
     def test_watchlist(self):
         watchlist = _run(self.user.get_watchlist())
