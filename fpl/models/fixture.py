ae0d25b896aa18c97e500ea6977360d8e1db9947
amosbastian <amosbastian@gmail.com>
2019-01-22 16:08:01 +0000

    Update Fixture class documentation

--- fpl/models/fixture.py
+++ fpl/models/fixture.py
@@ -10,7 +10,23 @@ def add_player(location, information):
 
 
 class Fixture():
-    """A class representing fixtures in the Fantasy Premier League."""
+    """A class representing fixtures in the Fantasy Premier League.
+
+    Basic usage::
+
+      >>> from fpl import FPL
+      >>> import aiohttp
+      >>> import asyncio
+      >>>
+      >>> async def main():
+      ...     async with aiohttp.ClientSession() as session:
+      ...         fpl = FPL(session)
+      ...         fixture = await fpl.get_fixture(1)
+      ...     print(fixture)
+      ...
+      >>> asyncio.run(main())
+      Arsenal vs. Man City - 10 Aug 19:00
+    """
     def __init__(self, fixture_information):
         for k, v in fixture_information.items():
             setattr(self, k, v)
@@ -26,71 +42,106 @@ class Fixture():
         return player_information
 
     def get_goalscorers(self):
-        """Returns all players who scored in the fixture."""
+        """Returns all players who scored in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("goals_scored")
 
     def get_assisters(self):
-        """Returns all players who made an assist in the fixture."""
+        """Returns all players who made an assist in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("assists")
 
     def get_own_goalscorers(self):
-        """Returns all players who scored an own goal in the fixture."""
+        """Returns all players who scored an own goal in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("own_goals")
 
     def get_yellow_cards(self):
-        """Returns all players who received a yellow card in the fixture."""
+        """Returns all players who received a yellow card in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("yellow_cards")
 
     def get_red_cards(self):
-        """Returns all players who received a red card in the fixture."""
+        """Returns all players who received a red card in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("red_cards")
 
     def get_penalty_saves(self):
-        """Returns all players who saved a penalty in the fixture."""
+        """Returns all players who saved a penalty in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("penalties_saved")
 
     def get_penalty_misses(self):
-        """Returns all players who missed a penalty in the fixture."""
+        """Returns all players who missed a penalty in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("penalties_missed")
 
     def get_saves(self):
-        """Returns all players who made a save in the fixture."""
+        """Returns all players who made a save in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("saves")
 
     def get_bonus(self):
-        """Returns all players who received bonus points in the fixture."""
+        """Returns all players who received bonus points in the fixture.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("bonus")
 
     def get_bps(self):
-        """Returns the bonus points of each player."""
+        """Returns the bonus points of each player.
+
+        :rtype: dict
+        """
         if not self.finished:
             return
 
         return self._get_players("bps")
+
+    def __str__(self):
+        return (f"{team_converter(self.team_h)} vs. "
+                f"{team_converter(self.team_a)} - "
+                f"{self.deadline_time_formatted}")
