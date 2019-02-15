2b7ea75e4d64858ca4b467376d62d81a0988a2be
Martin Šmíd <martin.smid94@seznam.cz>
2019-02-15 23:47:02 +0100

    Update test_classic_league

--- tests/test_classic_league.py
+++ tests/test_classic_league.py
@@ -1,4 +1,28 @@
+import aiohttp
+
+from fpl.models.classic_league import ClassicLeague
+
+
 class TestClassicLeague(object):
+    async def test_init(self, loop):
+        data = {
+            "new_entries": {"has_next": False, "number": 1, "results": []},
+            "league": {
+                "id": 1,
+                "leagueban_set": [],
+                "name": "Arsenal",
+                "short_name": "team-1",
+                "created": "2018-07-05T12:12:23Z",
+                "closed": False,
+            },
+        }
+        session = aiohttp.ClientSession()
+        classic_league = ClassicLeague(data, session)
+        assert classic_league._session is session
+        for k, v in data.items():
+            assert getattr(classic_league, k) == v
+        await session.close()
+
     async def test_classic_league(self, loop, classic_league):
         assert classic_league.__str__() == "Steem Fantasy League - 633353"
 
