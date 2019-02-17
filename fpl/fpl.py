189f65a70cb641bf49aa35627e57d29bd5cc467b
Martin Šmíd <martin.smid94@seznam.cz>
2019-02-17 20:01:12 +0100

    Update assertion on fpl.get_team team_id

--- fpl/fpl.py
+++ fpl/fpl.py
@@ -133,7 +133,7 @@ class FPL():
             19 - West Ham
             20 - Wolves
         """
-        assert 0 < team_id < 21, "Team ID must be in [1, 20]."
+        assert 0 < int(team_id) < 21, "Team ID must be a number between 1 and 20."
         url = API_URLS["teams"]
         teams = await fetch(self.session, url)
         team = next(team for team in teams if team["id"] == int(team_id))
