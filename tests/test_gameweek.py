8a7f74c9ed77a222dcd46d592783f3e172e11cf6
amosbastian <amos_bastian@hotmail.com>
2019-02-19 00:44:36 +0100

    Fix issues test_gameweek.py
    * Convert methods to static functions

--- tests/test_gameweek.py
+++ tests/test_gameweek.py
@@ -1,6 +1,8 @@
 class TestGameweek(object):
-    def test_gameweek(self, loop, gameweek):
+    @staticmethod
+    def test_gameweek(loop, gameweek):
         assert gameweek.__str__() == "Gameweek 6 - 22 Sep 11:30"
 
-    def test_fixtures(self, loop, gameweek):
+    @staticmethod
+    def test_fixtures(loop, gameweek):
         assert isinstance(gameweek.fixtures, list)
