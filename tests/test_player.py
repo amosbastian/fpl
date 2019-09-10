c1f2152247597d17dd4c42438b8a61b8bf66f078
Chris Musson <chris.musson@hotmail.com>
2019-09-10 17:23:28 +0100

    Added test for vapm

--- tests/test_player.py
+++ tests/test_player.py
@@ -11,3 +11,8 @@ class TestPlayer(object):
     def test_pp90(loop, player):
         pp90 = player.pp90
         assert isinstance(pp90, float)
+
+    @staticmethod
+    async def test_vapm(loop, player):
+        vapm = await player.vapm
+        assert isinstance(vapm, float)
