267c12a709c8f103f8af703d112be8d95baa0bd3
amosbastian <amosbastian@gmail.com>
2019-09-22 13:21:20 +0100

    Fix H2HLeague.__str__ & cleanup

--- fpl/models/h2h_league.py
+++ fpl/models/h2h_league.py
@@ -30,7 +30,7 @@ class H2HLeague():
         for k, v in league_information.items():
             setattr(self, k, v)
 
-    async def get_fixtures(self, gameweek=None):
+    async def get_fixtures(self, gameweek=None, page=1):
         """Returns a list of fixtures / results of the H2H league.
 
         Information is taken from e.g.:
@@ -38,30 +38,31 @@ class H2HLeague():
 
         :param gameweek: (optional) The gameweek of the fixtures / results.
         :type gameweek: string or int
+        :param page: (optional) The fixtures / results page.
+        :type page: string or int
         :rtype: list
         """
         if not self._session:
-            return
+            return []
 
         if not logged_in(self._session):
-            raise Exception("Not authorized to get h2h fixtures. Log in.")
+            raise Exception(
+                "Not authorised to get H2H fixtures. Log in first.")
 
-        url_query = ""
-        if gameweek:
-            url_query = f"event={gameweek}&"
-        page = 1
+        url_query = f"event={gameweek}&" if gameweek else ""
         has_next = True
         results = []
+
         while has_next:
-            print(API_URLS["league_h2h_fixtures"].format(self.league["id"], url_query, page))
-            fixtures = await asyncio.ensure_future(
-                fetch(self._session, API_URLS["league_h2h_fixtures"].format(self.league["id"], url_query, page)))
-            has_next = fixtures['has_next']
-            page = fixtures['page'] + 1
-            results.extend(fixtures['results'])
+            fixtures = await fetch(
+                self._session, API_URLS["league_h2h_fixtures"].format(
+                    self.league["id"], url_query, page))
+            results.extend(fixtures["results"])
+
+            has_next = fixtures["has_next"]
+            page += 1
 
         return results
 
-
-def __str__(self):
-    return f"{self.league['name']} - {self.league['id']}"
+    def __str__(self):
+        return f"{self.league['name']} - {self.league['id']}"
