2b5c9f29c6854fd80a80ca569bb082bc7a0ce943
Amos Bastian <amosbastian@gmail.com>
2021-04-04 16:54:39 +0200

    Bump version to 0.6.25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="fpl",
-    version="0.6.24",
+    version="0.6.25",
     packages=find_packages(),
     description="A Python wrapper for the Fantasy Premier League API",
     long_description=long_description,
