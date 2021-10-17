a3f54e957b9bc1cf43110f8fab7ed00ea392debb
HaydenMacDonald <hayden.macdonald.8778@gmail.com>
2021-10-17 21:56:44 +0100

    fix: changed .cfg section name to credentials

--- tests/temp_env_var.py
+++ tests/temp_env_var.py
@@ -4,8 +4,8 @@ config = configparser.ConfigParser()
 config.read("credentials.cfg")
 
 TEMP_ENV_VARS = {
-    "FPL_EMAIL": config["CREDS"]["FPL_EMAIL"],
-    "FPL_PASSWORD": config["CREDS"]["FPL_PASSWORD"]
+    "FPL_EMAIL": config["credentials"]["FPL_EMAIL"],
+    "FPL_PASSWORD": config["credentials"]["FPL_PASSWORD"]
 }
 
 ENV_VARS_TO_SUSPEND = [
