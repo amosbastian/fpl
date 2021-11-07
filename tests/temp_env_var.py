b8e43e51cb024ffa792d813c946988ade5f6bcc5
Amos Bastian <amosbastian@gmail.com>
2021-11-07 18:07:42 +0100

    chore(ci): add back credentials.cfg step

--- tests/temp_env_var.py
+++ tests/temp_env_var.py
@@ -4,6 +4,8 @@ import os
 config = configparser.ConfigParser()
 config.read(os.path.abspath("credentials.cfg"))
 
+print(config)
+
 TEMP_ENV_VARS = {
     "FPL_EMAIL": config["CREDENTIALS"]["FPL_EMAIL"] or os.getenv("FPL_EMAIL"),
     "FPL_PASSWORD": config["CREDENTIALS"]["FPL_PASSWORD"] or os.getenv("FPL_PASSWORD")
